46a81179ec7a74cd7ed9e563ff3a1f803bf7a442
KarloIsaac <qafqunam2001@gmail.com>
2017-11-23 10:15:52 -0600

    Could make a json request to the search api

--- Scripts.js
+++ Scripts.js
@@ -1,3 +1,19 @@
 function openRandomArticleLink() {
     window.open("https://en.wikipedia.org/wiki/Special:Random", "_blank");
 }
+
+
+function retrieveJsonQueryResult(searchTerm) {
+    var requestUrl = "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srlimit=500&origin=*&srsearch=";
+    requestUrl += encodeURIComponent(searchTerm);
+    var jsonResponse = requestUrl;
+    xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onreadystatechange  = function() {
+        if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) {
+            jsonResponse = JSON.parse(xmlHttpRequest.responseText);
+            console.log(jsonResponse);
+        }
+    }
+    xmlHttpRequest.open("GET", requestUrl, true);
+    xmlHttpRequest.send();
+}
